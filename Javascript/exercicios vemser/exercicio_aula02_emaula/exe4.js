8a2eef4a49acd52f122b356450be8a5e6f03247b
Paulo Sergio <paulosergiofragamarcos@gmail.com>
2022-03-08 16:00:17 -0300

    ajustes

--- Javascript/exercicios vemser/exercicio_aula02_emaula/exe4.js
+++ Javascript/exercicios vemser/exercicio_aula02_emaula/exe4.js
@@ -2,12 +2,13 @@
 
 var pergunta = parseInt(prompt("1 - Continuar perguntando / 2 - Parar de perguntar"));
 
-pergunta === 2 
-?alert("OK")
-:alert("Erro");
-pergunta = parseInt(prompt("1 - Continuar perguntando / 2 - Parar de perguntar"));
-
-while(pergunta === 1){
-    pergunta = 0;
+if(pergunta === 1){
+    while(pergunta === 1){
+        pergunta = 0;
+        pergunta = parseInt(prompt("1 - Continuar perguntando / 2 - Parar de perguntar"));
+    }
+}else if(pergunta === 2){
+}else{
+    alert("Erro!");
     pergunta = parseInt(prompt("1 - Continuar perguntando / 2 - Parar de perguntar"));
 }
