563d2ef7435efe490953bb8e9f47753fa06b8cd5
Paulo Sergio <paulosergiofragamarcos@gmail.com>
2022-03-23 21:15:57 -0300

    fix fix

--- React/portfolio_react/src/components/Perfil/Perfil.js
+++ React/portfolio_react/src/components/Perfil/Perfil.js
@@ -6,17 +6,22 @@ export default function Perfil() {
     function perfilRequest(){
         axios.get('https://api.github.com/users/PauloFra')
         .then((success)=>{
+          setLogin(success.data.login);
           setNome(success.data.name);
           setBio(success.data.bio);
           setEmpresa(success.data.company);
           setLocal(success.data.location);
+          setFoto(success.data.avatar_url);
+          console.log()
          
         })
     }
+    const [login , setLogin] = useState('');
     const [nome , setNome] = useState('');
     const [bio , setBio] = useState('');
     const [empresa , setEmpresa] = useState('');
     const [local , setLocal] = useState('');
+    const [foto , setFoto] = useState('');
     useEffect(()=>{
         perfilRequest()
     },[] );
@@ -24,11 +29,13 @@ export default function Perfil() {
     <main>
     <div>
         <div className={style.fotoENome}>
-          <img src={fotoPerfil} alt="" />
-          <h2>{nome}</h2>
+          <img src={foto} alt="" />
+          <h2>{login}</h2>
         </div>
         <div>
           <div className={style.basicInfo}>
+          <label htmlFor="">Nome:</label>
+          <input type="text" value={nome} />
           <label htmlFor="">Biografia:</label>
           <textarea name="" id="" value={bio}></textarea>
           <label htmlFor="">Empresa:</label>
